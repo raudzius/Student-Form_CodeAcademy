f43aca06f88ae7562d6708de317c3cfa2dfa361e
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-17 10:20:11 +0300

    feat/refactor: button to show/hide student submitted information making modal dynamic

--- app.js
+++ app.js
@@ -13,44 +13,87 @@ for (let i = 1; i <= 15; i++) {
     input.setAttribute(key, attrs[key]);
   }
 
+  if (i === 1) {
+    input.checked = true;
+  }
+
   span.append(label, input);
   divRadio.append(span);
 }
 //
 
+const spanModal = document.querySelector('span.modal');
+const backdrop = document.getElementById('backdrop');
 const form = document.querySelector('form');
 const divStudentList = document.getElementById('student-list');
 const inputLevel = document.getElementById('level');
 const levelLabelInput = document.getElementById('level-value');
+// Modal
+function displayModal(modalText) {
+  const h3 = document.createElement('h3');
+  h3.textContent = modalText;
 
-function addStudent(name, surname, age, phone, email, level, group, ...languages) {
-  // Student List item
-  const div = document.createElement('div');
-  div.className = 'student-item';
-
-  const pickedLanguages = languages.filter(language => language !== '');
-  const displayLanguages = pickedLanguages.length ? pickedLanguages.join(', ') + '.' : '';
-
-  div.innerHTML = `<h3>${name} ${surname}</h3>
-                   <p>Age: ${age}</p>
-                   <p>Tel: ${phone}</p>
-                   <p>E-mail: ${email}</p>
-                   <p>Level: ${level}</p>
-                   <p>Group: ${group}</p>
-                   <p>Programming languages: ${displayLanguages}</p>`;
-  divStudentList.prepend(div);
-  // Modal
-  const spanModal = document.querySelector('span.modal');
-  const backdrop = document.getElementById('backdrop');
-
-  spanModal.innerHTML = `<h3>Sukurtas Studentas: ${name} ${surname}</h3>`;
+  spanModal.append(h3);
   spanModal.style.display = 'block';
   backdrop.style.display = 'block';
 
   setTimeout(() => {
     spanModal.style.display = 'none';
     backdrop.style.display = 'none';
+    spanModal.children[0].remove();
   }, 5000);
+}
+
+function addStudent(name, surname, age, phone, email, level, group, ...languages) {
+  displayModal(`Sukurtas Studentas: ${name} ${surname}`);
+  // Student List item
+  const div = document.createElement('div');
+  div.className = 'student-item';
+
+  function turnLettersToStars(word) {
+    let stars = '';
+    for (i = 0; i < word.length; i++) {
+      stars += '*';
+    }
+    return stars;
+  }
+
+  const pickedLanguages = languages.filter(language => language !== '');
+  const displayLanguages = pickedLanguages.length ? pickedLanguages.join(', ') + '.' : '';
+  const h3 = document.createElement('h3');
+  h3.textContent = `${name} ${surname}`;
+  const p1 = document.createElement('p');
+  p1.textContent = 'Age: ' + age;
+  const p2 = document.createElement('p');
+  p2.textContent = 'Tel: ' + turnLettersToStars(phone);
+  const p3 = document.createElement('p');
+  p3.textContent = 'E-mail: ' + turnLettersToStars(email);
+  const p4 = document.createElement('p');
+  p4.textContent = 'Level: ' + level;
+  const p5 = document.createElement('p');
+  p5.textContent = 'Group: ' + group;
+  const p6 = document.createElement('p');
+  p6.textContent = 'Programming languages: ' + displayLanguages;
+  const btn = document.createElement('button');
+  btn.textContent = 'Rodyti asmens duomenis';
+
+  let isHidden = true;
+
+  btn.addEventListener('click', event => {
+    if (isHidden) {
+      p2.textContent = 'Tel: ' + phone;
+      p3.textContent = 'E-mail: ' + email;
+      btn.textContent = 'Slepti asmens duomenis';
+    } else {
+      p2.textContent = 'Tel: ' + turnLettersToStars(phone);
+      p3.textContent = 'E-mail: ' + turnLettersToStars(email);
+      btn.textContent = 'Rodyti asmens duomenis';
+    }
+    isHidden = !isHidden;
+  });
+
+  div.append(h3, p1, p2, p3, p4, p5, p6, btn);
+  divStudentList.prepend(div);
 }
 
 // Range input
@@ -77,6 +120,16 @@ form.addEventListener('submit', event => {
   const csharp = inputs.csharp.checked ? inputs.csharp.value : '';
 
   addStudent(name, surname, age, phone, email, level, group, javascript, python, java, csharp);
-  levelLabelInput.textContent = 1;
   form.reset();
+  levelLabelInput.textContent = 1;
+  inputs.gr[0].checked = true;
 });
+
+// 1. Vietoje asmens duomenų (el. paštas ir tel. nr) rodyti tik žvaigždutes „****".
+// 2. Kiekviename „student-item" elemente sukurti mygtuką „Rodyti asmens duomenis".
+// 3. Paspaudus šį mygtuką:
+// 3.1. Parodyti to studento el. paštą ir tel. nr.
+// 3.2. Pakeist mygtuko tekstą į „Slėpti asmens duomenis".
+// 4. Jeigu asmens duomenys yra rodomi, tai paspaudus mygtuką:
+// 4.1. Paslėpti asmens duomenis.
+// 4.2. Mygtuko tekstą pakeisti į „Rodyti asmens duomenis".
